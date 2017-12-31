ea00076df5d01256d53341a81417a86bed0be0e7
Liz Burton <liz.burton147@gmail.com>
2017-12-31 17:02:50 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,7 +29,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title
+  "SELECT series.title, characters.species
   FROM series
   JOIN characters
   ON series.id = characters.series_id
