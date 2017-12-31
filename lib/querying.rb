c3b4124f4e7cf55477ffe5c2801c1e5d27196126
Liz Burton <liz.burton147@gmail.com>
2017-12-31 18:04:11 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,6 +33,8 @@ def select_series_title_with_most_human_characters
   FROM series
   JOIN characters
   ON series.id = characters.series_id"
+  GROUP BY characters.species
+  ORDER BY COUNT(characters.species) DESC LIMIT 1";
 end
 
 def select_character_names_and_number_of_books_they_are_in
