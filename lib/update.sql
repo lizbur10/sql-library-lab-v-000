b05087799dcd0d35dd38ecdd8287795d7459c473
Liz Burton <liz.burton147@gmail.com>
2018-01-27 09:38:21 -0500

    Update update.sql

--- lib/update.sql
+++ lib/update.sql
@@ -1,1 +1,2 @@
-UPDATE characters SET species = "Martian" WHERE characters.id = 8;
+--UPDATE characters SET species = "Martian" WHERE characters.id = 8;
+UPDATE characters SET species = "Martian" WHERE id = (SELECT MAX(id) FROM characters);
